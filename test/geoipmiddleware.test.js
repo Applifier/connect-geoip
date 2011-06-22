ece999b4539ab9b0cc7a8de9043c5e33e13f1caf
Jaakko Lukkari <jaakko@applifier.com>
2011-06-22 16:28:29 +0300

    Added test for dummy country object

--- test/geoipmiddleware.test.js
+++ test/geoipmiddleware.test.js
@@ -17,5 +17,18 @@ module.exports = {
       { url: '/', headers:{'x-forwarded-for' : "8.8.8.8, proxy1, proxy2"}},
       { body: '{"country":{"country_code":"US","country_code3":"USA","country_name":"United States","continent_code":"NA"}}' });
 
+  },
+  'test ipv4 with local ip': function() {
+    var app = connect.createServer(
+      geoipMiddleware.geoipMiddleware(),
+      function(req, res, next) {
+        res.end(JSON.stringify(req.geoip));
+      }
+    );
+
+    assert.response(app,
+      { url: '/', headers:{'x-forwarded-for' : "127.0.0.1, proxy1, proxy2"}},
+      { body: '{"country":{"country_code":"--","country_code3":"---","country_name":"unknown","continent_code":"--"}}' });
+
   }
 }
