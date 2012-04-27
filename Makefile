d1d6c0c73bde1f7f4671f11037874bba67ac3349
Jaakko Lukkari <jaakko@applifier.com>
2012-04-27 15:01:31 +0300

    changed to use geoip-lite

---
+++ Makefile
@@ -0,0 +1,15 @@
+postinstall:
+	echo "Updating geoip data"; \
+	cd node_modules/geoip-lite/data; \
+	curl -O "http://koti.kapsi.fi/garo/GeoIPCountryCSV.zip"; \
+	unzip -u GeoIPCountryCSV.zip; \
+	rm GeoIPCountryCSV.zip; \
+	rm geoip-country.dat || echo "This is not a problem"; \
+	node ../lib/country-converter.js ./GeoIPCountryWhois.csv geoip-country.dat; \
+	rm GeoIPCountryWhois.csv
+
+
+test:
+	@./node_modules/.bin/expresso
+
+.PHONY: test
--- test/connect-geoip.test.js
+++ test/connect-geoip.test.js
@@ -6,29 +6,12 @@ var connect = require('connect')
 
 module.exports = {
   'test ipv4': function() {
-    var app = connect.createServer(
-      geoip.geoip(),
-      function(req, res, next) {
-        res.end(JSON.stringify(req.geoip));
-      }
-    );
-  
-    assert.response(app,
-      { url: '/', headers:{'x-forwarded-for' : "8.8.8.8, proxy1, proxy2"}},
-      { body: '{"country":{"country_name":"United States","country_code":"US","country_code3":"USA","continent_code":"NA"}}' });
+    var middleware = geoip.geoip();
 
-  },
-  'test ipv4 with an ip that is not in the database': function() {
-    var app = connect.createServer(
-      geoip.geoip(),
-      function(req, res, next) {
-        res.end(JSON.stringify(req.geoip));
-      }
-    );
+    var req = {connection: {remoteAddress: "213.243.175.65"}, headers:{}};
 
-    assert.response(app,
-      { url: '/', headers:{'x-forwarded-for' : "127.0.0.1, proxy1, proxy2"}},
-      { body: '{"country":{"country_code":"--","country_code3":"---","country_name":"unknown","continent_code":"--"}}' });
-
+    middleware(req, null, function () {
+      assert.equal(JSON.stringify(req.geoip), JSON.stringify({"country":{"country_code":"FI","country_code3":"---","country_name":"unknown","continent_code":"--"}}));
+    });
   }
-}
+};
